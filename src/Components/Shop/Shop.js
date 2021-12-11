7d03ca2a69e544f017f25a616388e2904fdc8834
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2021-12-11 11:55:40 +0600

    data attaching

---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import './Shop.css'
+
+const Shop = () => {
+
+    const [products,setProducts] = useState([]);
+
+    useEffect(()=>{
+        fetch('./products.JSON')
+        .then(res => res.json())
+        .then(data => console.log(data));
+    },[]);
+
+    return (
+        <div className='shopping-section'>
+            <div className='product-section'>
+                <h1>This is product</h1>
+            </div>
+            <div className='cart-section'>
+                <h1>this is cart</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
