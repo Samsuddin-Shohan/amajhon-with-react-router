091e34e957349e62f545f9652509f64df3a5c085
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2021-12-11 11:10:46 +0600

    nav added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import logo from '../../images/logo.png'
+import './Header.css'
 const Header = () => {
     return (
-        <div>
-            <img src={logo} alt="" />
+        <div className='header'>
+            <img className='logo' src={logo} alt="" />
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/order">Order Review</a>
+                <a href="/mange">Manage Inventory here</a>
+            </nav>
         </div>
     );
 };
