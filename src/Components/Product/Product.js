9a875224ad91e0cb45618ef936f3f55f5f10525b
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2021-12-12 04:57:03 +0600

    adding product

---
+++ src/Components/Product/Product.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    // console.log(props);
+    const {img, name, price, seller, stock} = props.product;
+    // console.log(name, price);
+    return (
+        <div className='product-list'>
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div className='product-description'>
+                <h3>{name}</h3>
+                <p><small>by: {seller}</small></p>
+                <p>${price}</p>
+                <p><small>only {stock} left in stock - order soon</small></p>
+
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default Product;
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
@@ -8,13 +9,15 @@ const Shop = () => {
     useEffect(()=>{
         fetch('./products.json')
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => setProducts(data));
     },[]);
 
     return (
         <div className='shopping-section'>
             <div className='product-section'>
-                <h1>This is product</h1>
+                {
+                    products.map(product => <Product product = {product}></Product> )
+                }
             </div>
             <div className='cart-section'>
                 <h1>this is cart</h1>
